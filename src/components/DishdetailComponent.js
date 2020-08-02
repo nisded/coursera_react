6f8b9c1305e8105a102e0409d8af2ace9ddf4dee
Alexey Rahuba <alexeyrahuba@yandex.ru>
2020-08-02 21:40:20 +0300

    using Fetch to communicate with REST API server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const minLength = (len) => (val) => val && (val.length >= len);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -98,7 +99,7 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
     return (
         <Card>
-            <CardImg src={dish.image} alt={dish.name} />
+            <CardImg src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
